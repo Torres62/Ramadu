aa3e99c381e725ab638dbf61e83f4a70af24bcbe
Torres62 <42254759+Torres62@users.noreply.github.com>
2018-09-13 20:31:07 -0300

    Update and rename AbrirPorta.h to Funcoes.h

---
+++ Door/temp/Funcoes.h
@@ -0,0 +1,11 @@
+int verificarTag (int a, int b);
+
+int cadastrarTag (int a, int b);
+
+int lerTag(int a, int b);
+
+int delTag(int a, int b);
+
+int closeDoor(int a, int b);
+
+int openDoor(int a, int b);
