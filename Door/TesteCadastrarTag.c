e04fd570dc635fad4b5259a02d15b6a4cda4fc3a
Eduwardo Horibe <40374938+eduhoribe@users.noreply.github.com>
2018-10-04 21:22:02 -0300

    Update TesteCadastrarTag.c

--- Door/TesteCadastrarTag.c
+++ Door/TesteCadastrarTag.c
@@ -4,4 +4,29 @@
 
 int main(int argc, char** argv[]) {
   
+  int uidTag[7], int uidLength;
+  char tempUidTag[7];
+  
+  int arraySize = 0;
+  
+  printf("\nDigite o tamanho da lista de Tags: ");
+  scanf("%i", &arraySize);
+  
+  Tag *tags = calloc(arraySize, sizeof(Tag));
+  
+  for(int i = 0; i < sizeArray; i++) {
+    
+    printf("\nDigite a UID da Tag: ");
+    scanf("%s", &tempUidTag);
+    
+    for(int j = 0; j < 7; j++) {
+      uidTag[i] = (int) tempUidTag[i];
+    }
+    
+    printf("\nDigite o peso da Tag: ");
+    scanf("%i", &uidLength);
+    
+    includeTag(uidTag, uidLength);
+  }
+  
 }
