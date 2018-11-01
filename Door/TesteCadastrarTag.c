ad1e9f00f4e444cf7627dd143f4b0e3b55337400
Torres62 <42254759+Torres62@users.noreply.github.com>
2018-11-01 20:56:12 -0300

    Update TesteCadastrarTag.c

--- Door/TesteCadastrarTag.c
+++ Door/TesteCadastrarTag.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv[]) {
   
   Tag *tags = calloc(arraySize, sizeof(Tag));
   
-  for(int i = 0; i < sizeArray; i++) {
+  for(int i = 0; i < arraySize; i++) {
     
     printf("\nDigite a UID da Tag: ");
     scanf("%s", &tempUidTag);
